7f75d5acd1f71ce0e90fdd0b461bad06fc41ede6
henokg <heni.gebrehiwot@gmail.com>
2023-04-19 20:04:28 +0300

    Section 8 - Edit User Get Action Method

--- IdentityManager/IdentityManager/Controllers/UserController.cs
+++ IdentityManager/IdentityManager/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityManager.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +37,27 @@ namespace IdentityManager.Controllers
             }
 
             return View(userList);
+        }
+        public IActionResult Edit(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (objFromDb == null)
+            {
+                return NotFound();
+            }
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == objFromDb.Id);
+            if (role != null)
+            {
+                objFromDb.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+            objFromDb.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(objFromDb);
         }
     }
 }
